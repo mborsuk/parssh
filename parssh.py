02c21fea68c4b7d6d7eda66363773d8b59155c00
Michael Borsuk <mrb@wiredsoul.org>
2014-10-21 02:41:05 -0700

    initial commit

---
+++ parssh.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from gevent import monkey; monkey.patch_all()
+from tornado.options import options, define
+import sys
+import json
+import pssh
+import logging
+
+define("config", type=str, metavar="config.py",
+        callback=lambda path: options.parse_config_file(path, final=False),
+        help="config file (optional)")
+define("hosts", type=str, multiple=True,
+        metavar="host1,host1,...",
+        help="comma-separated list of ssh hosts (required or specify hosts-file)")
+define("hosts_file", type=str, multiple=False,
+        metavar="hostfile.txt",
+        help="file containing new-line separated list of ssh hosts (required or specify hosts)")
+define("command", type=str, metavar="uptime",
+        help="command to execute on hosts (required)")
+define("sudo", type=bool, default=False, help="use sudo")
+
+def validate_options():
+    if not options.command:
+        options.print_help()
+        raise Error('missing required command option')
+    if not options.hosts and not options.hosts_file:
+        options.print_help()
+        raise Error('missing required option hosts or hosts-file')
+
+class Error(Exception):
+    pass
+
+if __name__ == '__main__':
+    try:
+        options.logging = 'warn'
+        options.add_parse_callback(validate_options)
+        options.parse_command_line()
+    except Error as e:
+        sys.stderr.write('{}\n'.format(str(e)))
+    logger = logging.getLogger()
+    if options.hosts_file:
+        with open(options.hosts_file) as file:
+            hosts = [ l.strip() for l in file.readlines() ]
+    client = pssh.ParallelSSHClient(hosts, pool_size=50)
+    greenlets = client.exec_command(options.command, sudo=options.sudo)
+    host_responses = {}
+    for g in greenlets:
+        response = client.get_stdout(g, return_buffers=True)
+        host = response.keys().pop()
+        retval = response[host]['exit_code']
+        stdout = [line for line in response[host]['stdout']]
+        stderr = [line for line in response[host]['stderr']]
+        logger.info('[{}][{}] {}'
+                .format(host, retval, ','.join(stdout)))
+        host_responses[host] = { 'exit_code': retval,
+            'stdout': stdout, 'stderr': stderr }
+    non_zero = []
+    for h in host_responses.keys():
+        if host_responses[h]['exit_code'] != 0:
+            non_zero.append(h)
+    json.dump({'hosts': host_responses,
+            'non_zero': non_zero}, sys.stdout)
---
+++ readme.md
@@ -0,0 +1,48 @@
+parssh
+======
+
+Another parallel ssh command executor
+
+Command line wrapper for [pkittenis/parallel-ssh](https://github.com/pkittenis/parallel-ssh).
+
+Powered by:
+* python
+* gevent
+* [paramiko](https://github.com/paramiko/paramiko)
+* [torando.options](http://tornado.readthedocs.org/en/latest/options.html)
+
+usage
+
+```
+% ./parssh.py --help
+Usage: ./parssh.py [OPTIONS]
+
+Options:
+
+  --command=uptime                 command to execute on hosts (required)
+  --config=config.py               config file (optional)
+  --help                           show this help information
+  --hosts=host1,host1,...          comma-separated list of ssh hosts (required
+                                   or specify hosts-file) (default [])
+  --hosts_file=hostfile.txt        file containing new-line separated list of
+                                   ssh hosts (required or specify hosts)
+  --sudo                           use sudo (default False)
+
+/opt/virtualenvs/parssh/lib/python2.7/site-packages/tornado/log.py options:
+
+  --log_file_max_size              max size of log files before rollover
+                                   (default 100000000)
+  --log_file_num_backups           number of log files to keep (default 10)
+  --log_file_prefix=PATH           Path prefix for log files. Note that if you
+                                   are running multiple tornado processes,
+                                   log_file_prefix must be different for each
+                                   of them (e.g. include the port number)
+  --log_to_stderr                  Send log output to stderr (colorized if
+                                   possible). By default use stderr if
+                                   --log_file_prefix is not set and no other
+                                   logging is configured.
+  --logging=debug|info|warning|error|none
+                                   Set the Python log level. If 'none', tornado
+                                   won't touch the logging configuration.
+                                   (default info)
+```
